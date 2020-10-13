a4e8e6f8bb40cbcfbed49be27413ca5c1aa23c60
cristopheSaraosV <46081150+cristopheSaraosV@users.noreply.github.com>
2020-10-13 01:17:50 -0500

    Primera versión del pokeApp con redux

---
+++ precache-manifest.65cd895dba55c2ff0089a3adb3630382.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8151ab786b3d5e7ab102c4467254cfec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91d8db759e0b6bd37bda",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "73043f2f087761d8b79b",
+    "url": "/static/js/2.4eb1714c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.4eb1714c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91d8db759e0b6bd37bda",
+    "url": "/static/js/main.e3f83975.chunk.js"
+  },
+  {
+    "revision": "59543281fd59440cf4f6",
+    "url": "/static/js/runtime-main.b618e835.js"
+  }
+]);
